ec1ff37896601bbe6a4ace0840c99959ed1132cf
dennyjohnk <denny.john@cherrylabsglobal.com>
2019-09-01 18:03:37 +0530

    first commit

---
+++ precache-manifest.4410a1a558c7b6b0f84c93de27a611a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07829937189e5cb9de20724aa3fa0a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54e0b499841c21f98a5",
+    "url": "/static/css/2.f06c6b30.chunk.css"
+  },
+  {
+    "revision": "ef0b29e87b910f2cc1bf",
+    "url": "/static/css/main.aa4c546b.chunk.css"
+  },
+  {
+    "revision": "a54e0b499841c21f98a5",
+    "url": "/static/js/2.ea9e9102.chunk.js"
+  },
+  {
+    "revision": "ef0b29e87b910f2cc1bf",
+    "url": "/static/js/main.409dfa3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "775019a04c4ad85d6512ae9916ef6401",
+    "url": "/static/media/image.775019a0.jpg"
+  },
+  {
+    "revision": "df803060910f2e04e3ffd98b2d0a6920",
+    "url": "/static/media/preview-icon.df803060.png"
+  }
+]);
